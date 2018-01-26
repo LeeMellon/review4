fb162c05bc191f0d93d1e9da4ab443b3f5854105
Ian Goodrich <iangoodrich@hotmail.com>
2018-01-26 10:56:53 -0800

    basic pizza constructor and first getprice prototype working.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,27 @@
+function FullPizza (toppings, cheese, sauce){
+  this.toppings = [["peperoni", 5], ["mushrooms", 3] ,["jalapenos", 4]] ;
+  this.cheees = ["mozz",2];
+  this.sauce = ["mar", 2];
+  this.price = 0;
+}
+
+function Customer (name, card, order){
+  this.name = name;
+  this.card = "";
+  this.order = [];
+}
+
+FullPizza.prototype.getPrice = function(){
+  var toppingsPrice =0;
+  this.toppings.forEach(function(item){
+   toppingsPrice += item[1];
+  });
+  return this.price += toppingsPrice;
+}
+
+var custy = new Customer ("Tony", "cash",);
+var pie1 = new FullPizza();
+
 $(function(){
 
 
