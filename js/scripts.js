64827546a7f0f4a92b7a23c7cd7ca87f93641e51
Ian Goodrich <iangoodrich@hotmail.com>
2018-01-26 17:12:52 -0800

    basic functionality added. Still have bug that causes not to work often. Debugger offering no insite as of yet. Must submit

--- js/scripts.js
+++ js/scripts.js
@@ -1,48 +1,77 @@
-function FullPizza (){
-  this.toppings = [["Topping: peperoni", 5], ["Topping: mushrooms", 3] ,["Topping: jalapenos", 4], ["cheese: Mozzerella", 4], ["Sauce: Marinarra", 3]] ;
-  this.invoice = [];
-  this.price = 0;
-}
-
-function Pizza (){
+function Pizza() {
   this.toppings = [];
   this.invoice = [];
   this.price = 0;
 }
+// 
+// function Customer(name, card, order) {
+//   this.name = name;
+//   this.card = "";
+//   this.order = [];
+// }
 
-function Customer (name, card, order){
-  this.name = name;
-  this.card = "";
-  this.order = [];
+Pizza.prototype.cleanInvoice = function(bulk) {
+  var toppingsDirty = []
+  var toppingsClean = "";
+  var priceClean = 0;
+  bulk.forEach(function(top) {
+    eachPiece = top.replace(/\"/g, "").split(",")
+    toppingsClean = eachPiece[0];
+    priceClean = parseInt(eachPiece[1]);
+  })
+  this.toppings.push([toppingsClean, priceClean]);
+  return this.toppings;
 }
 
-FullPizza.prototype.getPrice = function(){
-  var toppingsPrice =0;
-  this.toppings.forEach(function(item){
-   toppingsPrice += item[1];
+Pizza.prototype.getPrice = function() {
+  var toppingsPrice = 0;
+  this.toppings.forEach(function(item) {
+    toppingsPrice += parseFloat(item[1]);
   });
   return this.price += toppingsPrice;
 }
 
-FullPizza.prototype.getInvoice = function(){
+Pizza.prototype.getInvoice = function() {
   var toppingInvoice = [];
-  this.toppings.forEach(function(item){
+  this.toppings.forEach(function(item) {
     var itemName = item[0];
     var itemPrice = item[1];
-    var invoiceItem = itemName +"        for  $ " + itemPrice + ".00"
+    var invoiceItem = itemName + "        for  $ " + itemPrice + ".00"
     toppingInvoice.push(invoiceItem)
-    console.log(itemName, itemPrice, toppingInvoice);
   })
   return this.invoice = toppingInvoice;
 }
 
+// var custy = new Customer("Tony", "cash", );
+var pie1 = new Pizza();
 
-var custy = new Customer ("Tony", "cash",);
-var pie1 = new FullPizza();
+$(function() {
+  $("#sizeForm").change(function(event) {
+    var size = $("input:radio[name=size]:checked").val();
+    event.preventDefault();
+    $(".sizeChoice").fadeOut(900, "swing", "complete");
+    $("#" + size + "Pizza").delay(950).fadeIn(900, "swing");
 
-$(function(){
-
-
-
-
+    $("#orderBttn").click(function(event) {
+      event.preventDefault();
+      var bulk = [];
+      debugger
+      $('input[name=pizzaParts]:checked').map(function() {
+        var coarseFormInput = ($(this).val());
+        console.log("1");
+        bulk.push(coarseFormInput);
+        console.log("2");
+        pie1.cleanInvoice(bulk);
+        var recipt = pie1.getInvoice();
+        console.log("3");
+        var price = pie1.getPrice();
+        var invoiceEl = document.getElementById("invoice");
+        var priceEl = document.getElementById("price");
+        invoiceEl.innerHTML = recipt;
+        priceEl.innerHTML = "$ " + price;
+        $("#" + size + "Pizza").fadeOut(900, "swing");
+        $(".costCont").delay(950).fadeTo(900, 1);
+      })
+    })
+  })
 });
